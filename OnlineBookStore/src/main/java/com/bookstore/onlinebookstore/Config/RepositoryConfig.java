a22545ce9bf9055d563cf91d4ec68620cfdcda9c
hamza19956 <hamzabmw2008@hotmail.com>
2020-04-30 15:03:53 +0200

    Repository configuration to expose id

---
+++ OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/Config/RepositoryConfig.java
@@ -0,0 +1,27 @@
+package com.bookstore.onlinebookstore.Config;
+
+import com.bookstore.onlinebookstore.Model.Book;
+import com.bookstore.onlinebookstore.Model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+import javax.swing.text.html.parser.Entity;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+    @Autowired
+    EntityManager entityManager;
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        //manual solution
+            //config.exposeIdsFor(Book.class);
+            //config.exposeIdsFor(Category.class);
+
+        //or automatic for all new created entity
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+    }
+}
